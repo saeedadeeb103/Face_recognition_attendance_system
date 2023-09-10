920454fff3d797dfc9b63e46211171feb4bd49f3
Saeed <saeedbenadeeb@gmail.com>
2023-09-10 23:25:21 +0200

    Face Recognition Finished

--- encodeGen.py
+++ encodeGen.py
@@ -12,4 +12,26 @@ employesID = []
 
 for path in PathList:
     imgList.append(cv.imread(os.path.join(FolderPath, path)))
-    path
+    employesID.append(os.path.splitext(path)[0])
+
+print(employesID)
+
+
+def FindEncodeings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+print("Encoding Started ....")
+KnownEncodings = FindEncodeings(imgList)
+KnownEncodingswithIDs = [KnownEncodings, employesID]
+print("Encoding Compelete")
+
+file = open("EncodeFile.p", 'wb')
+
+pickle.dump(KnownEncodingswithIDs, file)
+file.close()
+print("File Saved")
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2 as cv 
 import os
-
+import pickle
+import face_recognition
+import numpy as np 
+import cvzone
 imgsz = (640, 480)
 cap = cv.VideoCapture(0, cv.CAP_DSHOW)
 cap.set(3, imgsz[0])
@@ -12,16 +15,42 @@ imgBackground = cv.imread('resources/background.png')
 FolderModePath = 'resources/Modes'
 ModePathList = os.listdir(FolderModePath)
 imgModeList = []
-
-print(ModePathList)
 for path in ModePathList:
     imgModeList.append(cv.imread(os.path.join(FolderModePath, path)))
-print(len(imgModeList))
 
+# Load the encoding file 
+print("Loading Encoded File .......")
+file = open('EncodeFile.p', 'rb')
+KnownEncodingwithIDs  = pickle.load(file)
+file.close()
+
+KnownEncodings, employesID = KnownEncodingwithIDs
+# print(employesID)
+print("Encoded File Loaded")
 while True:
     success, img = cap.read()
+
+    imgSize = cv.resize(img, (0,0), None, 0.25, 0.25)
+    imgSize = cv.cvtColor(imgSize, cv.COLOR_BGR2RGB)
+
+    faceCurrentFrame = face_recognition.face_locations(imgSize)
+    encodeCurrentFrame = face_recognition.face_encodings(imgSize, faceCurrentFrame)
     imgBackground[162:162+480, 55:55+640] = img
     imgBackground[44:44+633, 808:808+414] = imgModeList[1]
+
+    for encodeFace, faceLoc in zip(encodeCurrentFrame, faceCurrentFrame):
+        matches = face_recognition.compare_faces(KnownEncodings, encodeFace)
+        Face_Dis = face_recognition.face_distance(KnownEncodings, encodeFace)
+        matchIndex = np.argmin(Face_Dis)
+
+        if matches[matchIndex]:
+            # print(employesID[matchIndex])
+            # print("Known Faces Detected.")
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+
+            bbox = 55+x1, 162+y1, x2-x1, y2-y1 
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt= 0 )
     cv.imshow("Face Attendance", imgBackground)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
